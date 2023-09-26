5b3f06adfbed7f315c587b44a83804a04715d303
SOHAM PATHAK <120130000+pathaksoham2003@users.noreply.github.com>
2023-09-26 14:50:18 +0530

    26/09/2023

--- blood_donation/members/urls.py
+++ blood_donation/members/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('members/', views.members , name ="members"),
+    path('login/', views.login , name ="members"),
+    path('register/',views.register , name="register")
 ]
--- blood_donation/members/views.py
+++ blood_donation/members/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 
-def members(request):
-  template = loader.get_template('myfirst.html')
+def login(request):
+  template = loader.get_template('login.html')
+  return HttpResponse(template.render())
+def register(request):
+  template = loader.get_template('register.html')
   return HttpResponse(template.render())
